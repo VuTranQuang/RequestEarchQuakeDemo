0016255009edc906730278e527c6d6dd6c57abdd
Vu <vutran7993@gmail.com>
2019-04-25 08:19:04 +0700

    Initial Commit

---
+++ RequestApiEarthQuakeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RequestApiEarthQuakeDemo
+//
+//  Created by Vu on 4/25/19.
+//  Copyright © 2019 Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
